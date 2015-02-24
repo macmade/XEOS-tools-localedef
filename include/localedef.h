15451ee5e3f4cc18439f0f9219438efffcd1de12
macmade <macmade@xs-labs.com>
2015-02-24 01:46:49 +0100

    Added the new copyright notices.

--- include/localedef.h
+++ include/localedef.h
@@ -59,6 +59,11 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  ******************************************************************************/
 
+/*!
+ * @author          Jean-David Gadina
+ * @copyright       (c) 2010-2015, Jean-David Gadina - www.xs-labs.com
+ */
+
 #ifndef TOOLS_LOCALEDEF_H
 #define TOOLS_LOCALEDEF_H
 
